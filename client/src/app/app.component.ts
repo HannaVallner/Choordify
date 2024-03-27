4af2a2c7b2fe0243dbfddb812b24255f53873f81
Hanna Vallner <hanna.vallner@gmail.com>
2024-03-27 13:16:38 +0200

    Added backend: server.js

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core'; 
+import { ApiService } from './api.service'; 
+
+@Component({ 
+	selector: 'app-root', 
+	templateUrl: './app.component.html', 
+	styleUrls: ['./app.component.scss'] 
+}) 
+export class AppComponent implements OnInit { 
+	title = 'Choordify'; 
+	message: any; 
+
+	constructor(private apiService: ApiService) { }; 
+  
+	ngOnInit() { 
+		this.apiService.getMessage().subscribe(data => { 
+			this.message = data; 
+		}); 
+	} 
+}
---
+++ server.js
@@ -0,0 +1,139 @@
+const express = require('express'); 
+const request = require('request');
+const crypto = require('crypto');
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+const app = express(); 
+
+var client_id = 'a52b1c6ae851463b8614c146866ecf5d';
+var client_secret = '19aff404760f490db5e964da47134a3d';
+var redirect_uri = 'http://localhost:3000/callback'; 
+var basePath = "http://localhost:4200"
+
+const generateRandomString = (length) => {
+    return crypto
+    .randomBytes(60)
+    .toString('hex')
+    .slice(0, length);
+}
+
+var stateKey = 'spotify_auth_state';
+
+
+app.use(express.static(__dirname + '/client'))
+   .use(cors())
+   .use(cookieParser());
+
+
+app.get("/login", function (req, res) {
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // redirects user to Spotify login page with scopes of information access
+  var scope =
+    "user-read-private user-top-read playlist-read-private playlist-read-collaborative playlist-modify-public";
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      })
+  );
+});
+
+/* CALLBACK URL from Spotify verification */
+// when verification is complete, render homepage
+app.get("/callback/", function (req, res) {
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
+  
+    if (state === null || state !== storedState) {
+      // if state does not match, error
+      res.redirect(
+        "/#" +
+          querystring.stringify({
+            error: "state_mismatch",
+          })
+      );
+    } else {
+      // gets and returns authentication token
+      res.clearCookie(stateKey);
+      var authOptions = {
+        url: "https://accounts.spotify.com/api/token",
+        form: {
+          code: code,
+          redirect_uri: redirect_uri,
+          grant_type: "authorization_code",
+        },
+        headers: {
+          Authorization:
+            "Basic " +
+            new Buffer(client_id + ":" + client_secret).toString("base64"),
+        },
+        json: true,
+      };
+  
+      request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+  
+          // redirects back to browser frontend
+          res.redirect(basePath + "/?authorized=true#" + access_token);
+        } else {
+          res.redirect(
+            basePath +
+              "/?" +
+              querystring.stringify({
+                error: "invalid_token#error",
+              })
+          );
+        }
+      });
+    }
+});
+
+
+app.get('/refresh_token', function(req, res) {
+
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { 
+        'content-type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')) 
+      },
+      form: {
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token
+      },
+      json: true
+    };
+  
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+        res.send({
+          'access_token': access_token,
+          'refresh_token': refresh_token
+        });
+      }
+    });
+});
+
+// route for handling requests from the Angular client 
+app.get('/api/message', (req, res) => { 
+	res.json({ message: 
+			'Hello from the Express server!' }); 
+}); 
+
+
+app.listen(3000, () => { 
+	console.log('Server listening on port 3000'); 
+});
