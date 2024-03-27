f667054bc60e0d62969b2aafff301a82fee2b8b4
Hanna Vallner <hanna.vallner@gmail.com>
2024-03-27 17:15:07 +0200

    Added initial Spotify service

---
+++ client/src/app/spotify.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) { }
+
+  getUser(token: string) {
+    return this.http.get('https://api.spotify.com/v1/me', {
+      headers: { Authorization: 'Bearer ' + token },
+    })
+  }
+
+  getPlaylists(token: string) {
+    return this.http.get('https://api.spotify.com/v1/me/playlists?limit=50&offset=0', {
+      headers: { Authorization: 'Bearer ' + token},
+    })
+  }
+}
