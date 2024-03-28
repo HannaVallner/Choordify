f043dc5f511a67b76db123f617631ac2721adace
Hanna Vallner <hanna.vallner@gmail.com>
2024-03-28 17:13:50 +0200

    Changed username to be visible on navbar instead of homepage

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { SpotifyService } from '../spotify.service';
+
 
 @Component({
   selector: 'app-home',
@@ -8,27 +7,9 @@ import { SpotifyService } from '../spotify.service';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit{
-  message: any
-  userInfo: any;
-  token = '';
-  username = '';
-  displayName = '';
-
-
-  constructor(private spotify: SpotifyService, private router: Router) {}
+  constructor() {}
 
   ngOnInit() {
-    const storedToken = sessionStorage.getItem('token');
-
-    if (storedToken != null) {
-      this.token = storedToken; 
-      this.spotify.getUserInfo(this.token).then(userInfoObservable => {
-        userInfoObservable.subscribe(userInfo => {
-        this.userInfo = userInfo;
-      
-      });
-    });
   }
-}
 
 }
