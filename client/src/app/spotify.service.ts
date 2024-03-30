b2bc9cbd5bc8bc86e05700710ebba4160862dff8
Hanna Vallner <hanna.vallner@gmail.com>
2024-03-30 03:04:32 +0200

    Added new required methods to spotify service and updated scope accordingly

--- client/src/app/spotify.service.ts
+++ client/src/app/spotify.service.ts
@@ -4,26 +4,76 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+
 export class SpotifyService {
 
   constructor(private http: HttpClient) { }
 
-  
+  // Returns user's info  
   async getUserInfo(token: string) {
     return this.http.get('https://api.spotify.com/v1/me', {
       headers: { Authorization: 'Bearer ' + token },
     })
   }
 
+  // Returns user's playlists
   getPlaylists(token: string) {
     return this.http.get('https://api.spotify.com/v1/me/playlists?limit=50&offset=0', {
-      headers: { Authorization: 'Bearer ' + token},
+      headers: { Authorization: 'Bearer ' + token },
     })
   }
 
+  // Returns a playlist's tracks
+  getPlaylistTracks(token: string, playlistId: string, offset: string) {
+    return this.http.get('https://api.spotify.com/v1/playlists/' + playlistId + '/tracks?limit=50&offset=' + offset, {
+      headers: { Authorization: 'Bearer ' + token },
+    })
+  }
+
+  // Returns a track's features
+  getTrackFeatures(token: string, trackId: string) {
+    return this.http.get('https://api.spotify.com/v1/audio-features/' + trackId, {
+      headers: { Authorization: 'Bearer ' + token },
+    })
+  }
+
+  // Returns several tracks' features
+  getTracksFeaturer(token: string, trackIds: string) {
+    return this.http.get('https://api.spotify.com/v1/audio-features?' + trackIds, {
+      headers: { Authorization: 'Bearer ' + token },
+    })
+  }
+
+  // Adds tracks to a given playlist
+  addPlaylistTracks(token: string, playlistId: string, trackURIs: string) {
+    return this.http.post('https://api.spotify.com/v1/playlists/' + playlistId + '/tracks', {
+      uris: trackURIs },
+      { headers: { Authorization: 'Bearer ' + token },
+    })
+  }
+
+  // Removes tracks from a given playlist
+  // tracks = An array of objects containing Spotify URIs of the tracks or episodes to remove.
+  // For example: { "tracks": [{ "uri": "spotify:track:4iV5W9uYEdYUVa79Axb7Rh" },{ "uri": "spotify:track:1301WleyT98MSxVHPZCA6M" }] }
+  removePlaylistTracks(token: string, playlistId: string, trackURIs: object[]) {
+    return this.http.delete('https://api.spotify.com/v1/playlists/' + playlistId + '/tracks', {
+      headers: { Authorization: 'Bearer ' + token },
+      body: { tracks: trackURIs }
+    });
+  }
+
+  // Creates a new playlist
+  createPlaylist(token: string, userId: string, playlistName: string) {
+    return this.http.post('https://api.spotify.com/v1/users/' + userId +'/playlists', {
+      headers: { Authorization: 'Bearer ' + token }, 
+      body: { name: playlistName}
+    });
+  }
+
+  // Returns tracks which match a given input
   searchForTracks(token: string, input: string) {
     return this.http.get('https://api.spotify.com/v1/search?q=' + input + '&type=track', {
-      headers: { Authorization: 'Bearer ' + token},
+      headers: { Authorization: 'Bearer ' + token },
     })
   }
 }
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/spotify/auth", function (req, res) {
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
   var scope =
-    "user-read-private user-read-email user-top-read playlist-read-private playlist-read-collaborative playlist-modify-public";
+    "user-read-private user-read-email user-top-read playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private";
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
